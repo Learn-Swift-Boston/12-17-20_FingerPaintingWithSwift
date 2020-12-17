fed9a50a72e05c6238ba12b6df73f48e180bc747
Matt Dias <matt.dias6@gmail.com>
2020-12-17 18:54:33 -0500

    Initial Commit

---
+++ FingerPainter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FingerPainter
+//
+//  Created by Matthew Dias on 12/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
